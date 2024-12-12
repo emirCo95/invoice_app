c94a391c180d7464caee821def257d7cdcbeed5f
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-12-12 17:07:03 -0500

    create dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,61 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+
+import { Plus } from 'lucide-react';
+
+export default function Dashboard() {
+  return (
+    <main className="flex flex-col justify-center h-screen text-center max-w-5xl mx-auto gap-6 my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <p>
+          <Button variant="ghost">
+            <Plus className="h-4 w-4" />
+            Create Invoice
+          </Button>
+        </p>
+      </div>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px] p-4">Date</TableHead>
+            <TableHead className="p-4">Customer</TableHead>
+            <TableHead className="p-4">Email</TableHead>
+            <TableHead className="text-center p-4">Status</TableHead>
+            <TableHead className="text-right p-4">Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium text-left p-4">
+              <span className="font-semibold">10/31/2024</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span className="font-semibold">John Doe</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span>johndoe@gmail.com</span>
+            </TableCell>
+            <TableCell className="text-center p-4">
+              <Badge className="rounded-full">Open</Badge>
+            </TableCell>
+            <TableCell className="text-right p-4">
+              <span>$250.00</span>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </main>
+  );
+}
