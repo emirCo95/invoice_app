0d5dad037a7adeffb367bf2d344fe532306645a5
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-12-18 20:49:34 -0500

    create SubmitButton component

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,18 +1,41 @@
+'use client';
+
+import { SyntheticEvent, useState, startTransition } from 'react';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 
+import SubmitButton from '@/components/SubmitButton';
+
 import { createAction } from '@/app/actions';
 
 export default function NewInvoice() {
+  const [state, setState] = useState('ready');
+
+  async function handleOnSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    if (state === 'pending') return;
+    setState('pending');
+    const target = event.target as HTMLFormElement;
+    startTransition(async () => {
+      const formData = new FormData(target);
+      await createAction(formData);
+    });
+  }
+
   return (
     <main className="flex flex-col justify-center h-full max-w-5xl mx-auto gap-6 my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold">Create Invoice</h1>
       </div>
 
-      <form action={createAction} className="grid gap-4 max-w-xs">
+      <form
+        action={createAction}
+        onSubmit={handleOnSubmit}
+        className="grid gap-4 max-w-xs"
+      >
         <div className="">
           <Label className="block mb-2 font-semibold text-sm" htmlFor="name">
             Billing Name
@@ -41,7 +64,7 @@ export default function NewInvoice() {
           <Textarea id="description" name="description"></Textarea>
         </div>
         <div className="">
-          <Button className="w-full font-semibold">Submit</Button>
+          <SubmitButton />
         </div>
       </form>
     </main>
---
+++ src/components/SubmitButton.tsx
@@ -0,0 +1,7 @@
+import { Button } from './ui/button';
+
+const SubmitButton = () => {
+  return <Button className="w-full font-semibold">Submit</Button>;
+};
+
+export default SubmitButton;
